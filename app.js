d9118d84c2f93971a0c195cb31737738025dcef6
龚航宇 <785567192@qq.com>
2023-06-30 22:28:03 +0800

    feat: 解决跨域问题

--- app.js
+++ app.js
@@ -1,8 +1,25 @@
 const express = require("express");
-const app = express();
-
+const cors = require("cors");
 const weatherRoutes = require("./router");
 
-app.use("", weatherRoutes);
+const app = express();
+
+app.use((req, res, next) => {
+  //设置请求头
+  res.set({
+    "Access-Control-Allow-Credentials": true,
+    "Access-Control-Max-Age": 1728000,
+    "Access-Control-Allow-Origin": req.headers.origin || "*",
+    "Access-Control-Allow-Headers": "X-Requested-With,Content-Type",
+    "Access-Control-Allow-Methods": "PUT,POST,GET,DELETE,OPTIONS",
+    "Content-Type": "application/json; charset=utf-8",
+  });
+  req.method === "OPTIONS" ? res.status(204).end() : next();
+});
+
+app.use(express.json());
+app.use(cors());
+
+app.use("/api", weatherRoutes);
 
 app.listen(4000, () => console.log(`服务器启动成功, 监听 ${4000} 端口`));
--- router.js
+++ router.js
@@ -2,7 +2,6 @@ const express = require("express");
 const router = express.Router();
 const qs = require("querystring");
 const service = require("./server");
-const http = require("http");
 
 router.get("/weatherSearch", (req, res, next) => {
   const search_key = qs.parse(req._parsedUrl.query);
